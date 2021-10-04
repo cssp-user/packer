f8e1f5432011a339df7181a90a9488f28d8ba796
Cloud User <centos@ip-172-31-16-8.ec2.internal>
2021-10-04 00:26:27 +0000

    dd

---
+++ bash-scripts/hardening.sh
@@ -0,0 +1,11 @@
+#sudo yum update -y
+#sudo yum upgrade -y
+#sudo yum install git -y
+#sudo yum install wget -y
+#sudo yum install curl -y
+#sudo yum install unzip -y
+sudo yum install epel-release -y
+sudo cp /home/centos/ansible.repo /etc/yum.repos.d/
+sudo yum install ansible -y
+sudo yum install docker -y
+
